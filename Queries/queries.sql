dcebf74a24497720d6702ea5a03ebda5b89dccbd
Nightlymist <nathalieaouellette@gmail.com>
2022-10-10 10:57:10 -0400

    Fixing mistake

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -135,23 +135,3 @@ WHERE d.dept_name IN ('Sales', 'Development');
 
 SELECT * FROM teams_emp;
 
---Deliverables #2
---CREATING TABLE mentorship_eligibility
-SELECT DISTINCT ON (e.emp_no)e.emp_no,
-	   e.first_name,
-	   e.last_name,
-	   e.birth_date,
-	   de.from_date,
-	   de.to_date,
-	   t.title
-INTO mentorship_eligibility
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (de.to_date = '9999-01-01')
-	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
-
-SELECT * FROM mentorship_eligibility;
